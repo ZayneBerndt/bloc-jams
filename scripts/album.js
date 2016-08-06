927768f2c220220a1069043cbf21b0716274460c
Zayne <berndt.zayne@gmail.com>
2016-08-06 15:36:22 +1000

    Add Albums And Toggle Feature

--- scripts/album.js
+++ scripts/album.js
@@ -1,35 +1,49 @@
 var albumPicasso = {
-    title: 'The Colors',
-    artist: 'Pablo Picaso',
-    label: 'Cubism',
-    year: '1881',
-    albumArtUrl: 'assets/images/album_covers/01.png',
-    songs: [
-        {title: 'Blue', duration: '4:26'},
-        {title: 'Green', duration: '3:14'},
-        {title: 'Red', duration: '5:01'},
-        {title: 'Pink', duration: '3:21'},
-        {title: 'Magenta', duration: '2:14'},
-    ]
-};
+     title: 'The Colors',
+     artist: 'Pablo Picasso',
+     label: 'Cubism',
+     year: '1881',
+     albumArtUrl: 'assets/images/album_covers/01.png',
+     songs: [
+         { title: 'Blue', duration: '4:26' },
+         { title: 'Green', duration: '3:14' },
+         { title: 'Red', duration: '5:01' },
+         { title: 'Pink', duration: '3:21'},
+         { title: 'Magenta', duration: '2:15'}
+     ]
+ };
+ 
+ var albumMarconi = {
+     title: 'The Telephone',
+     artist: 'Guglielmo Marconi',
+     label: 'EM',
+     year: '1909',
+     albumArtUrl: 'assets/images/album_covers/20.png',
+     songs: [
+         { title: 'Hello, Operator?', duration: '1:01' },
+         { title: 'Ring, ring, ring', duration: '5:01' },
+         { title: 'Fits in your pocket', duration: '3:21'},
+         { title: 'Can you hear me now?', duration: '3:14' },
+         { title: 'Wrong phone number', duration: '2:15'}
+     ]
+ };
 
-var albumMarconi = {
-   title: 'The Telephone',
-    artist: 'Gulielmo Marconi',
-    label: 'EM',
-    year: '1909',
-    albumArtUrl: 'assets/images/album_covers/20.png',
-    songs: [
-        {title: 'Hello Operator', duration: '1:01'},
-        {title: 'Ring, ring, ring', duration: '4:02'},
-        {title: 'Fits in your pocket', duration: '4:10'},
-        {title: 'Can you hear me?', duration: '5:20'},
-        {title: 'Wrong phone number', duration: '2:06'},
-    ]
-};
-
+var albumRockStar = {
+     title: 'Planet Rock',
+     artist: 'Astronaut Guitarist',
+     label: 'EM',
+     year: '2020',
+     albumArtUrl: 'assets/images/album_covers/19.png',
+     songs: [
+         { title: 'Rock on Pluto?', duration: '1:01' },
+         { title: 'Mocking Mars', duration: '5:01' },
+         { title: 'Stoned Jupiter ', duration: '3:21'},
+         { title: 'Victorius Venus', duration: '3:14' },
+         
+     ]
+ };
 var createSongRow = function(songNumber, songName, songLength) {
-    var template =
+     var template =
         '<tr class="album-view-song-item">'
       + '  <td class="song-item-number">' + songNumber + '</td>'
       + '  <td class="song-item-title">' + songName + '</td>'
@@ -40,22 +54,22 @@ var createSongRow = function(songNumber, songName, songLength) {
      return template;
  };
 
-var setCurrentAlbum = function(album) {
      var albumTitle = document.getElementsByClassName('album-view-title')[0];
      var albumArtist = document.getElementsByClassName('album-view-artist')[0];
      var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
      var albumImage = document.getElementsByClassName('album-cover-art')[0];
      var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
-     
+
+var setCurrentAlbum = function(album) {
      albumTitle.firstChild.nodeValue = album.title;
      albumArtist.firstChild.nodeValue = album.artist;
      albumReleaseInfo.firstChild.nodeValue = album.year + ' ' + album.label;
      albumImage.setAttribute('src', album.albumArtUrl);
  
-    
+     
      albumSongList.innerHTML = '';
  
-   
+     
      for (var i = 0; i < album.songs.length; i++) {
          albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
      }
@@ -63,4 +77,14 @@ var setCurrentAlbum = function(album) {
  
  window.onload = function() {
      setCurrentAlbum(albumPicasso);
- };
+
+     var albums = [albumPicasso, albumMarconi, albumRockStar];
+     var index = 1;
+albumImage.addEventListener("click", function(event) {
+   setCurrentAlbum(albums[index]); 
+    index++;
+    if (index == albums.length) {
+        index = 0;
+    }
+});
+      };
