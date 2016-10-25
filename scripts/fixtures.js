88d7ca9e27dea2d77eaf9b4386c547c1123d5cf5
Zayne Berndt <berndt.zayne@gmail.com>
2016-10-25 19:46:22 +1000

    Debug

--- scripts/fixtures.js
+++ scripts/fixtures.js
@@ -1,44 +1,44 @@
 var albumPicasso = {
-     title: 'The Colors',
-     artist: 'Pablo Picasso',
-     label: 'Cubism',
-     year: '1881',
-     albumArtUrl: 'assets/images/album_covers/01.png',
-     songs: [
-         { title: 'Blue', duration: '4:26' },
-         { title: 'Green', duration: '3:14' },
-         { title: 'Red', duration: '5:01' },
-         { title: 'Pink', duration: '3:21'},
-         { title: 'Magenta', duration: '2:15'}
-     ]
- };
- 
- var albumMarconi = {
-     title: 'The Telephone',
-     artist: 'Guglielmo Marconi',
-     label: 'EM',
-     year: '1909',
-     albumArtUrl: 'assets/images/album_covers/20.png',
-     songs: [
-         { title: 'Hello, Operator?', duration: '1:01' },
-         { title: 'Ring, ring, ring', duration: '5:01' },
-         { title: 'Fits in your pocket', duration: '3:21'},
-         { title: 'Can you hear me now?', duration: '3:14' },
-         { title: 'Wrong phone number', duration: '2:15'}
-     ]
- };
+    name: 'The Colors',
+    artist: 'Pablo Picasso',
+    label: 'Cubism',
+    year: '1881',
+    albumArtUrl: 'assets/images/album_covers/01.png',
+    songs: [
+      { name: 'Blue', length: 161.75, audioUrl: '/assets/music/blue' },
+      { name: 'Green', length: 103.96, audioUrl: '/assets/music/green' },
+      { name: 'Red', length: 268.45, audioUrl: '/assets/music/red' },
+      { name: 'Pink', length: 153.14, audioUrl: '/assets/music/pink' },
+      { name: 'Magenta', length: 374.22, audioUrl: '/assets/music/magenta' }
+    ]
+};
 
-var albumRockStar = {
-     title: 'Planet Rock',
-     artist: 'Astronaut Guitarist',
-     label: 'EM',
-     year: '2020',
-     albumArtUrl: 'assets/images/album_covers/19.png',
-     songs: [
-         { title: 'Rock on Pluto?', duration: '1:01' },
-         { title: 'Mocking Mars', duration: '5:01' },
-         { title: 'Stoned Jupiter ', duration: '3:21'},
-         { title: 'Victorius Venus', duration: '3:14' },
-         
-     ]
- };
+var albumMarconi = {
+    name: 'The Telephone',
+    artist: 'Guglielmo Marconi',
+    label: 'EM',
+    year: '1909',
+    albumArtUrl: 'assets/images/album_covers/20.png',
+    songs: [
+      { name: 'Hello, Operator?', length: '1:01' },
+      { name: 'Ring, ring, ring', length: '5:01' },
+      { name: 'Fits in your pocket', length: '3:21' },
+      { name: 'Can you hear me now?', length: '3:14' },
+      { name: 'Wrong phone number', length: '2:15' }
+    ]
+};
+
+var albumTheDoors = {
+    name: 'The Doors',
+    artist: 'The Doors',
+    label: 'Elektra Records',
+    year: '1966',
+    albumArtUrl: 'assets/images/album_covers/22.png',
+    songs: [
+        { name: 'Break On Through (to the Other Side)', length: '2:26' },
+        { name: 'Soul Kitchen', length: '3:35' },
+        { name: 'Crystal Ship', length: '2:30'},
+        { name: 'Twentieth Century Fox', length: '2:34' },
+        { name: 'Alabama Song', length: '3:20'}
+    ]
+};
